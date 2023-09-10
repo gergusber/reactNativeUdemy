456cb2db570b6764faecbdedaf4782da6376b0d1
German Bertea <gerbertea@gmail.com>
2023-09-10 21:09:52 +0200

    add gradient and image as background

--- seccion4-ts/my-game/App.tsx
+++ seccion4-ts/my-game/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
 import StartGameScreen from './screens/StartGameScreen'
 import { LinearGradient } from 'expo-linear-gradient';
 
@@ -8,7 +8,13 @@ export default function App() {
     <LinearGradient
       colors={["#4e0329", '#ddb52f']}
       style={styles.rootScreen}>
-      <StartGameScreen />
+      <ImageBackground
+        source={require('./assets/images/background.png')}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}>
+        <StartGameScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
@@ -17,7 +23,7 @@ const styles = StyleSheet.create({
   rootScreen: {
     flex: 1,
   },
-  container: {
-    // flex: 100
+  backgroundImage: {
+    opacity: 0.15
   }
 });
